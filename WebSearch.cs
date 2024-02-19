7a6c2a63740b9e24712246f36a24e2a123c45721
tkefauver <tkefauver@gmail.com>
2024-02-19 13:36:02 -0500

    Added missing cultures. Updated for MonkeyPaste.Common.Plugin 1.0.2.0

--- WebSearch.cs
+++ WebSearch.cs
@@ -1,15 +1,16 @@
 ﻿using MonkeyPaste.Common;
 using MonkeyPaste.Common.Plugin;
 using System;
-using System.Data;
 using System.Diagnostics;
 using System.Linq;
 
 namespace WebSearch {
     public class WebSearch : MpIAnalyzeComponent {
+        const string URI_PARAM_ID = "1";
         public MpAnalyzerPluginResponseFormat Analyze(MpAnalyzerPluginRequestFormat req) {
+            Resources.Culture = new System.Globalization.CultureInfo(req.culture);
             if (req != null &&
-                req.GetRequestParamStringValue(1) is string uri) {
+                req.GetParamValue<string>(URI_PARAM_ID) is string uri) {
                 try {
                     using (var myProcess = new Process()) {
                         myProcess.StartInfo.UseShellExecute = true;
@@ -20,9 +21,9 @@ namespace WebSearch {
                 catch (Exception ex) {
                     return new MpAnalyzerPluginResponseFormat() {
                         userNotifications = new[] {
-                            new MpPluginUserNotificationFormat() {
+                            new MpUserNotification() {
                                 NotificationType = MpPluginNotificationType.PluginResponseError,
-                                Title = "Web Search Error",
+                                Title = Resources.ErrorLabel,
                                 Body = ex.Message,
                                 Detail = uri,
                                 IconSourceObj = MpBase64Images.Error
